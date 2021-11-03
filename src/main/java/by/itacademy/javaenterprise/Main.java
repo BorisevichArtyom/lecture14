47670d2187d67f7f42e13525c602d6a4270ab426
ieronim777 <71116171+ieronim777@users.noreply.github.com>
2021-11-03 23:58:48 +0300

    second commit

--- src/main/java/by/itacademy/javaenterprise/Main.java
+++ src/main/java/by/itacademy/javaenterprise/Main.java
@@ -22,34 +22,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws DAOException {
-        User user = User.builder().email("griiib@tut.by")
-                .password("Bicep").firstName("Denis")
-                .lastName("Samusenko").balanceAmount(new BigDecimal("123.44"))
-                .role(Role.ATHLETE).build();
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        UserDAOImpl daoImpl = context.getBean(UserDAOImpl.class);
 
-//        daoImpl.addUser(user);
-////        daoImpl.deleteUser(user);
-//        daoImpl.addUser(user);
-
-//        user.setFirstName("Aleks");
-//        System.out.println(daoImpl.getUserById(1));
-//        daoImpl.updateUser(user);
-
-        System.out.println(daoImpl.getAllUsersPagination(3, 1));
-
-        System.out.println(daoImpl.getUserById(4));
-
-//        Training training = Training.builder().id(10).date(LocalDateTime.now()).userID(1).build();
-//        System.out.println(training);
-//        TrainingDAOImpl trainingDAOImpl = context.getBean(TrainingDAOImpl.class);
-//        trainingDAOImplem.addTraining(training);
-//         trainingDAOImplem.deleteTraining(training);
-//        training.setDate(LocalDateTime.of(2014, 9, 19, 14, 5, 20));
-//        trainingDAOImpl.updateTraining(training);
-//        System.out.println(trainingDAOImpl.getAllTrainingsPagination(2,1));
-//             Main runner= context.getBean(Main.class);
-//             runner.executeSmth(2);
     }
 }
